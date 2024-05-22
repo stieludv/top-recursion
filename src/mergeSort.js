56af9111220a25445cf349220e358c99bc4f0ca0
stieludv <19951864+stieludv@users.noreply.github.com>
2024-05-23 01:50:54 +0200

    feat: mergeSort using recursion

---
+++ src/mergeSort.js
@@ -0,0 +1,20 @@
+
+const mergeSort = (arr) => {
+    if (arr.length === 1) {
+        return arr;
+    }
+    const mid = Math.ceil(arr.length / 2);
+    const leftArr = arr.slice(0, mid);
+    const rightArr = arr.slice(mid);
+    return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+const merge = (leftArr, rightArr) => {
+    const sortedArr = [];
+    while (leftArr.length && rightArr.length) {
+        leftArr[0] > rightArr[0] ? sortedArr.push(rightArr.shift()) : sortedArr.push(leftArr.shift());
+    }
+    return [...sortedArr, ...leftArr, ...rightArr];
+}
+
+module.export = mergeSort;
