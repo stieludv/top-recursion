41fdbd4b103012cd8b74e7b9244fbb078267b0c1
stieludv <19951864+stieludv@users.noreply.github.com>
2024-05-22 21:55:15 +0200

    feat: iterative fibonacci finder

---
+++ src/fibonacci.js
@@ -0,0 +1,25 @@
+
+
+const fibs = (n) => {
+    let fib1 = 0;
+    let fib2 = 1;
+    // Two fibonacci numbers are already declared, i = 2
+    for (let i = 2; i < n; i++) {
+        let newFib = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = newFib;
+    }
+    return fib1 + fib2;
+}
+
+const fibsRec = (n) => {
+    
+}
+
+
+console.log(fibs(7));
+console.log(fibs(37));
+
+console.log(fibsRec(7));
+console.log(fibsRec(37));
+
